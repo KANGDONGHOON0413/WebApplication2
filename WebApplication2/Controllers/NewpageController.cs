c3e5c98cde56b0ed731f708ef16c876e5810b0c4
KANGDONGHOON0413 <kmerits@gmail.com>
2022-09-21 19:56:22 +0900

    초기 커밋.

---
+++ WebApplication2/Controllers/NewpageController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    public class NewpageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Newpage()
+        {
+            return View();
+        }
+
+        public IActionResult Newpage2()
+        {
+            return View();
+        }
+    }
+}
